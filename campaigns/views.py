66dc97faea5a4aa3d0c6896c566409f243caecea
KdPisda <pisdak79@gmail.com>
2020-02-29 16:02:04 +0530

    update: login working static

--- campaigns/views.py
+++ campaigns/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
+def login(request):
+    return render(request, "login.html")
+    # return HttpResponse("Login World")
+
+
+def register(request):
+    return HttpResponse("Registr World")
--- croundfunding/urls.py
+++ croundfunding/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from campaigns.views import login, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', login),
+    path('register', register),
 ]
